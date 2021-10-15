c3266e334db22f6fbecebc0a883dd9d2af2411c7
ThumViu <appdevelopp2121@gmail.com>
2021-10-15 15:47:49 -0600

    first commit

---
+++ Vivin/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Vivin
+//
+//  Created by Abraham De Leon on 10/15/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
